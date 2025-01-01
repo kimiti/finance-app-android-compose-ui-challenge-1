a70835734d192d0148f120fa51826737c35da139
kimiti <kimitikevin@gmail.com>
2025-01-01 09:30:00 +0300

    add splash screen image

--- app/src/main/java/com/example/financeapp/Activities/IntroActivity/screens/IntroScreen.kt
+++ app/src/main/java/com/example/financeapp/Activities/IntroActivity/screens/IntroScreen.kt
@@ -1,10 +1,38 @@
 package com.example.financeapp.Activities.IntroActivity.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.financeapp.R
 
+@Preview
 @Composable
 fun IntroScreen(
-    onStartClick: () -> Unit
-){
+    onStartClick: () -> Unit = {}
+) {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        val (image, btn, title) = createRefs()
 
+        Image(
+            painter = painterResource(R.drawable.intro_pic),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .constrainAs(image) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                }
+                .fillMaxSize()
+        )
+    }
 }
