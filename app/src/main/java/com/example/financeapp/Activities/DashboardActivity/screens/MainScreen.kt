c82c6be1757ad2990d5bf8117cdd2c7b44650838
kimiti <kimitikevin@gmail.com>
2025-01-02 09:30:00 +0300

    adds header section

--- app/src/main/java/com/example/financeapp/Activities/DashboardActivity/screens/MainScreen.kt
+++ app/src/main/java/com/example/financeapp/Activities/DashboardActivity/screens/MainScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.financeapp.Activities.DashboardActivity.components.HeaderSection
 import com.example.financeapp.Domain.ExpenseDomain
 
 @Composable
@@ -29,7 +30,11 @@ fun MainScreen(
                 .fillMaxSize()
                 .padding(bottom = 70.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp)
-        ) { }
+        ) {
+            item {
+                HeaderSection()
+            }
+        }
     }
 }
 
