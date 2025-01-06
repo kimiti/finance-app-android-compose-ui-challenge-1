42c3917ffe8efdc69896bbfb7c06e93d89a8d033
kimiti <kimitikevin@gmail.com>
2025-01-06 08:00:00 +0300

    adds NavigationBar class

--- app/src/main/java/com/example/financeapp/Activities/DashboardActivity/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/financeapp/Activities/DashboardActivity/components/BottomNavigationBar.kt
@@ -1,8 +1,22 @@
 package com.example.financeapp.Activities.DashboardActivity.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.NavigationBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.financeapp.R
 
 @Composable
-fun BottomNavigationBar(){
-    
+fun BottomNavigationBar(onItemSelected: (Int) -> Unit, modifier: Modifier = Modifier) {
+    NavigationBar(
+        containerColor = colorResource(R.color.lightBlue),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(80.dp)
+    ) {
+
+    }
 }
