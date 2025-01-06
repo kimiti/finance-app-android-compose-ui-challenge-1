a34135dde5c971f47ee442df65039b3129b0d74a
kimiti <kimitikevin@gmail.com>
2025-01-06 10:00:00 +0300

    adds NavigationBarItem class

--- app/src/main/java/com/example/financeapp/Activities/DashboardActivity/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/financeapp/Activities/DashboardActivity/components/BottomNavigationBar.kt
@@ -2,10 +2,14 @@ package com.example.financeapp.Activities.DashboardActivity.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.financeapp.R
 
@@ -17,6 +21,13 @@ fun BottomNavigationBar(onItemSelected: (Int) -> Unit, modifier: Modifier = Modi
             .fillMaxWidth()
             .height(80.dp)
     ) {
-
+        NavigationBarItem(
+            selected = true,
+            onClick = { onItemSelected(R.id.wallet) },
+            icon = {
+                Icon(painter = painterResource(R.drawable.wallet), contentDescription = null)
+            },
+            label = { Text("Home") }
+        )
     }
 }
