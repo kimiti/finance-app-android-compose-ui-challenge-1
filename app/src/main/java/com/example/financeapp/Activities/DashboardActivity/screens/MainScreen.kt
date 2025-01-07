8fd190ee6dbd5f0b60125c740fc1767a509ccfa7
kimiti <kimitikevin@gmail.com>
2025-01-07 08:30:00 +0300

    adds NavigationBar Items

--- app/src/main/java/com/example/financeapp/Activities/DashboardActivity/screens/MainScreen.kt
+++ app/src/main/java/com/example/financeapp/Activities/DashboardActivity/screens/MainScreen.kt
@@ -5,19 +5,24 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.financeapp.Activities.DashboardActivity.components.ActionButtonRow
+import com.example.financeapp.Activities.DashboardActivity.components.BottomNavigationBar
 import com.example.financeapp.Activities.DashboardActivity.components.CardSection
 import com.example.financeapp.Activities.DashboardActivity.components.ExpenseItem
 import com.example.financeapp.Activities.DashboardActivity.components.HeaderSection
 import com.example.financeapp.Domain.ExpenseDomain
+import com.example.financeapp.R
 
 @Composable
 fun MainScreen(
@@ -41,6 +46,17 @@ fun MainScreen(
             items(expenses){item ->
                 ExpenseItem(item) }
         }
+        BottomNavigationBar(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .height(80.dp),
+            onItemSelected = {itemId ->
+                if(itemId == R.drawable.wallet){
+
+                }
+            }
+        )
+
     }
 }
 
