374ec523501919f07a4e0683df68dec4709de51d
kimiti <kimitikevin@gmail.com>
2025-01-07 08:10:00 +0300

    adds NavigationBar Items

--- app/src/main/java/com/example/financeapp/Activities/DashboardActivity/MainActivity.kt
+++ app/src/main/java/com/example/financeapp/Activities/DashboardActivity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,16 +12,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.financeapp.Activities.DashboardActivity.screens.MainScreen
+import com.example.financeapp.ViewModel.MainViewModel
 import com.example.financeapp.ui.theme.FinanceAppTheme
 
 class MainActivity : ComponentActivity() {
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FinanceAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    MainScreen(expenses = mainViewModel.loadData(), onCardClick = {})
                 }
             }
         }
