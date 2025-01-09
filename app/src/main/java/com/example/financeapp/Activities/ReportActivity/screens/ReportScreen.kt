12088e0b3772ce6dca911549e2884f24424a830c
kimiti <kimitikevin@gmail.com>
2025-01-09 08:30:00 +0300

    adds BudgetDomain data class

--- app/src/main/java/com/example/financeapp/Activities/ReportActivity/screens/ReportScreen.kt
+++ app/src/main/java/com/example/financeapp/Activities/ReportActivity/screens/ReportScreen.kt
@@ -1,2 +1,12 @@
 package com.example.financeapp.Activities.ReportActivity.screens
 
+import androidx.compose.runtime.Composable
+import com.example.financeapp.Domain.BudgetDomain
+
+@Composable
+fun ReportScreen(
+    budgets: List<BudgetDomain>,
+    onBack: () -> Unit
+){
+
+}
