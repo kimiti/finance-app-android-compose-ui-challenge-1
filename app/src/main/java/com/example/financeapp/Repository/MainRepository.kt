0b061edea814fc0e944fb950a77fbc0396da1497
kimiti <kimitikevin@gmail.com>
2025-01-10 08:00:00 +0300

    adds dummy Budget data to repo

--- app/src/main/java/com/example/financeapp/Repository/MainRepository.kt
+++ app/src/main/java/com/example/financeapp/Repository/MainRepository.kt
@@ -1,5 +1,6 @@
 package com.example.financeapp.Repository
 
+import com.example.financeapp.Domain.BudgetDomain
 import com.example.financeapp.Domain.ExpenseDomain
 
 class MainRepository {
@@ -8,5 +9,11 @@ class MainRepository {
         ExpenseDomain("McDonald", 77.82, "img2", "16 jun 2025 13:57"),
         ExpenseDomain("Cinema", 23.47, "img3", "16 jun 2025 20:45"),
         ExpenseDomain("restaurant", 573.12, "img4", "15 jun 2025 22:18")
+    )
+
+    val budget = mutableListOf(
+        BudgetDomain("Home load", 1200.0, 80.0),
+        BudgetDomain("subscription", 550.0, 10.0),
+        BudgetDomain("car loan", 800.0, 30.0)
     )
 }
