06b2a9525f48460d19a2180849fbe93447bda650
kimiti <kimitikevin@gmail.com>
2025-01-11 08:30:00 +0300

    adds budgetdomain data class

--- app/src/main/java/com/example/financeapp/Domain/BudgetDomain.kt
+++ app/src/main/java/com/example/financeapp/Domain/BudgetDomain.kt
@@ -5,5 +5,5 @@ import java.io.Serializable
 data class BudgetDomain(
     val title: String,
     val price: Double= 0.0,
-    val percentage: Double= 0.0,
+    val percent: Double= 0.0,
 ): Serializable
