31c8953497dd38a4d0773a4eaffd574751b7ff8d
kimiti <kimitikevin@gmail.com>
2025-01-15 08:00:00 +0300

    adds StatCard function

--- app/src/main/java/com/example/financeapp/Activities/ReportActivity/components/CenterStatsCard.kt
+++ app/src/main/java/com/example/financeapp/Activities/ReportActivity/components/CenterStatsCard.kt
@@ -1,2 +1,37 @@
 package com.example.financeapp.Activities.ReportActivity.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+
+@Composable
+fun CenterStatCard(modifier: Modifier = Modifier) {
+    ConstraintLayout(
+        modifier = modifier
+            .background(Color.White, shape = RoundedCornerShape(24.dp))
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = RoundedCornerShape(24.dp)
+            )
+            .padding(12.dp)
+    ) {
+        val (progressBar, totalTxt, totalLabel,
+            IncomeIcon, InomeLabel, IncomeValue,
+            expenseIcon, expenseLabel, expenseValue) = createRefs()
+    }
+}
+
+
+@Preview
+@Composable
+fun CenterStatCardPreview() {
+    CenterStatCard()
+}
