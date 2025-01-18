68ebe14f6a911a7e9658ac29b5b4cb0bbcebb4d6
kimiti <kimitikevin@gmail.com>
2025-01-18 08:30:00 +0300

    adds CircularProgressBar component to main screen

--- app/src/main/java/com/example/financeapp/Activities/ReportActivity/screens/ReportScreen.kt
+++ app/src/main/java/com/example/financeapp/Activities/ReportActivity/screens/ReportScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,6 +13,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.financeapp.Activities.ReportActivity.components.CenterStatCard
 import com.example.financeapp.Activities.ReportActivity.components.GradientHeader
 import com.example.financeapp.Domain.BudgetDomain
 
@@ -29,7 +32,7 @@ fun ReportScreen(
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                    bottom.linkTo(bottomBarRef.top)
+//                    bottom.linkTo(parent.top)
                 },
             onBack = onBack
         )
@@ -50,7 +53,7 @@ fun ReportContent(
             ConstraintLayout(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(250.dp)
+                    .height(420.dp)
             ) {
                 val (header, card) = createRefs()
                 GradientHeader(
@@ -64,6 +67,16 @@ fun ReportContent(
                         },
                     onBack = onBack
                 )
+                CenterStatCard(modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(horizontal = 24.dp)
+                    .constrainAs(card){
+                        top.linkTo(header.bottom)
+                        bottom.linkTo(header.bottom)
+                        start.linkTo(header.start)
+                        end.linkTo(header.end)
+                    })
             }
         }
 
