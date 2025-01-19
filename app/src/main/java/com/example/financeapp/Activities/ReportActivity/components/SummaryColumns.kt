36a6a7e032ce49862883b1ba0d6c5f2a40ac50ce
kimiti <kimitikevin@gmail.com>
2025-01-19 08:30:00 +0300

    adds SummaryColumn fun params

--- app/src/main/java/com/example/financeapp/Activities/ReportActivity/components/SummaryColumns.kt
+++ app/src/main/java/com/example/financeapp/Activities/ReportActivity/components/SummaryColumns.kt
@@ -1,2 +1,16 @@
 package com.example.financeapp.Activities.ReportActivity.components
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun SummaryColumn(
+    title: String,
+    value: String,
+    percent: String,
+    percentColor: Color,
+    modifier: Modifier = Modifier
+){
+
+}
