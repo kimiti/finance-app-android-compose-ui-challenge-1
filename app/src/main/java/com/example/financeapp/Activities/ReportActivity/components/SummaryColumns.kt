04a03f6b3e25ca755dee35dd080946bd5938bb55
kimiti <kimitikevin@gmail.com>
2025-01-20 08:00:00 +0300

    adds SummaryColumn dummy data

--- app/src/main/java/com/example/financeapp/Activities/ReportActivity/components/SummaryColumns.kt
+++ app/src/main/java/com/example/financeapp/Activities/ReportActivity/components/SummaryColumns.kt
@@ -1,9 +1,14 @@
 package com.example.financeapp.Activities.ReportActivity.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -16,7 +21,34 @@ import com.example.financeapp.R
 
 @Composable
 fun SummaryColumn(modifier: Modifier = Modifier) {
-
+    Row(
+        modifier = modifier.height(IntrinsicSize.Min),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        SummaryColumn(
+            title = "Total Balance",
+            value = "$3,452.23",
+            percent = "+25%",
+            percentColor = Color(0xff2d9738),
+            modifier = Modifier.weight(1f)
+        )
+        VerticalDivider()
+        SummaryColumn(
+            title = "Income",
+            value = "$165.21",
+            percent = "-10%",
+            percentColor = Color(0xff2d9738),
+            modifier = Modifier.weight(1f)
+        )
+        VerticalDivider()
+        SummaryColumn(
+            title = "saving",
+            value = "$1,542.22",
+            percent = "+16%",
+            percentColor = Color(0xff2d9738),
+            modifier = Modifier.weight(1f)
+        )
+    }
 }
 
 
