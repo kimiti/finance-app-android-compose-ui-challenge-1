b8d693b1d68e9ff1f34c55dc8cfe233ffe39bf84
kimiti <kimitikevin@gmail.com>
2025-01-22 08:00:00 +0300

    adds BudgetItem section

--- app/src/main/java/com/example/financeapp/Activities/ReportActivity/components/BudgetItem.kt
+++ app/src/main/java/com/example/financeapp/Activities/ReportActivity/components/BudgetItem.kt
@@ -1,7 +1,40 @@
 package com.example.financeapp.Activities.ReportActivity.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.financeapp.Domain.BudgetDomain
 
 @Composable
-fun BudgetItem() {
+fun BudgetItem(budget: BudgetDomain, index: Int) {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(110.dp)
+            .background(Color.White, RoundedCornerShape(16.dp))
+            .padding(horizontal = 24.dp, vertical = 4.dp)
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = RoundedCornerShape(12.dp)
+            )
+    ) {
+
+    }
+}
+
+@Preview
+@Composable
+fun BudgetItemPreview(){
+    val budget = BudgetDomain(title = "Food", price = 100.0, percent = 50.0)
+    BudgetItem(budget = budget, index = 0)
 }
