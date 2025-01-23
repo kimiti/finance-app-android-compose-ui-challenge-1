a3bac2fb157d43f95d84c35be9c2fbc3a5ad8baf
kimiti <kimitikevin@gmail.com>
2025-01-23 09:30:00 +0300

    adds budgetitem to main screen

--- app/src/main/java/com/example/financeapp/Activities/ReportActivity/screens/ReportScreen.kt
+++ app/src/main/java/com/example/financeapp/Activities/ReportActivity/screens/ReportScreen.kt
@@ -1,20 +1,28 @@
 package com.example.financeapp.Activities.ReportActivity.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.financeapp.Activities.DashboardActivity.components.BottomNavigationBar
+import com.example.financeapp.Activities.ReportActivity.components.BudgetItem
 import com.example.financeapp.Activities.ReportActivity.components.CenterStatCard
 import com.example.financeapp.Activities.ReportActivity.components.GradientHeader
 import com.example.financeapp.Activities.ReportActivity.components.SummaryColumns
@@ -40,6 +48,8 @@ fun ReportScreen(
                 },
             onBack = onBack
         )
+
+        BottomNavigationBar()
     }
 }
 
@@ -95,6 +105,29 @@ fun ReportContent(
                 )
                 .padding(8.dp)
         ) }
+
+
+        item{
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top=16.dp)
+                    .padding(horizontal = 24.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    "My Budget",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 19.sp,
+                    color = colorResource(R.color.darkBlue)
+                )
+                Text("Edit", color = colorResource(R.color.darkBlue))
+            }
+        }
+
+        itemsIndexed(budgets){index, item ->
+            BudgetItem(budget = item, index = index)
+        }
     }
 }
 
