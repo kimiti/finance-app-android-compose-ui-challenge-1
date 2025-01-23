90d9268d39e5fcd246d9fb4638d63196435bec49
kimiti <kimitikevin@gmail.com>
2025-01-23 09:00:00 +0300

    adds CircularProgress bar UI

--- app/src/main/java/com/example/financeapp/Activities/ReportActivity/components/BudgetItem.kt
+++ app/src/main/java/com/example/financeapp/Activities/ReportActivity/components/BudgetItem.kt
@@ -1,5 +1,6 @@
 package com.example.financeapp.Activities.ReportActivity.components
 
+import android.icu.text.DecimalFormat
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,12 +8,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.financeapp.Domain.BudgetDomain
 import com.example.financeapp.R
@@ -46,6 +50,27 @@ fun BudgetItem(budget: BudgetDomain, index: Int) {
                     start.linkTo(parent.start, margin = 16.dp)
                     bottom.linkTo(parent.bottom)
                 }
+        )
+
+        Text(
+            budget.title,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            modifier = Modifier.constrainAs(title){
+                top.linkTo(progress.top, margin = 8.dp)
+                start.linkTo(progress.end, margin = 8.dp)
+            }
+        )
+
+        Text(
+            "$${DecimalFormat("###,###,###,###").format(budget.price)} /Month",
+            color = colorResource(R.color.grey),
+            modifier = Modifier.constrainAs(price){
+                top.linkTo(title.bottom)
+                start.linkTo(title.start)
+                bottom.linkTo(progress.bottom)
+            }
         )
 
     }
