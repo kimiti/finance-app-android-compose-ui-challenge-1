2636d51bebc1b2208d9b800565fda3ac203fe248
kimiti <kimitikevin@gmail.com>
2025-01-25 08:30:00 +0300

    adds DashboardActivity

--- app/src/main/java/com/example/financeapp/Activities/DashboardActivity/MainActivity.kt
+++ app/src/main/java/com/example/financeapp/Activities/DashboardActivity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.financeapp.Activities.DashboardActivity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.financeapp.Activities.DashboardActivity.screens.MainScreen
+import com.example.financeapp.Activities.ReportActivity.ReportActivity
 import com.example.financeapp.ViewModel.MainViewModel
 import com.example.financeapp.ui.theme.FinanceAppTheme
 
@@ -24,7 +26,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             FinanceAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(expenses = mainViewModel.loadData(), onCardClick = {})
+                    MainScreen(expenses = mainViewModel.loadData(), onCardClick = {
+                        startActivity(Intent(this, ReportActivity::class.java))
+                    })
+
                 }
             }
         }
